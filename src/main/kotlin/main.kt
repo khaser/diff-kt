638de048a4e6975c2f4a10f421b4ef1a0ba20c9c
khaser <a-horohorin@mail.ru>
2021-09-20 20:44:59 +0300

    README.md written. Project compiled to diff-kt.jar

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,50 @@ import output.printDiff
 import java.io.File
 import kotlin.system.exitProcess
 
+
+val help = """
+NAME
+    diff-kt - compare two files line by line on kotlin
+SYNOPSIS
+    java -jar diff-kt [OPTIONS] fileA fileB
+OPTIONS
+    [-h or --help] - show this help
+    [-f ${'$'}FILE or --file ${'$'}FILE] - redirect output from standard output to ${'$'}FILE
+    [-w ${'$'}NUM or --width ${'$'}NUM] - set column width to ${'$'}NUM
+    [-s ${'$'}SIGN or --sign ${'$'}SIGN] - use short short sign of diff block instead of default long.
+        ${'$'}SIGN can take the following values:
+            long(Default)
+            Example: Deleted strings from 24 to 28
+            short
+            Example: -24-28
+            none Do not print sign
+        Example: "-24-28" instead of "Deleted strings from 24 to 28"
+    [-c ${'$'}MODE or --common ${'$'}MODE] set printing mode for common parts of files
+    [-d ${'$'}MODE or --diff ${'$'}MODE] set printing mode for different parts of files
+        ${'$'}MODE can take the following values:
+            split(Default)
+            Example: text from file A                        ||text from file B
+            series
+            Example: text from file a
+                     text from file b
+            none Nothing is output
+    [-o or --context] Enable context mode for printing difference. This option ignoring --common and --diff
+        Example:
+        Common part 1
+        text from file A 1                                   ||text from file B 1
+        Common part 2
+        -------------------------------------------------------------------------
+        Common part 3
+        text from file A 2                                   ||text from file B 2
+        Common part 4
+        -------------------------------------------------------------------------
+    [-b ${'$'}NUM or --border ${'$'}NUM] Set context border to ${'$'}NUM lines. 5 by default.
+EXIT CODES
+    0 - SUCCESS code, files identical
+    1 - SUCCESS code, files differ
+    2 - FAIL code, Input\output failed
+""".trimIndent()
+
 fun main(args: Array<String>) {
     //Preparsing argument
     if (args.size < 2) {
@@ -23,8 +67,7 @@ fun main(args: Array<String>) {
     val options = parseAllKeys(argsKeys)
 
     if (options[Option.HELP] == "true") {
-        val helpFile = File("help.txt")
-        helpFile.readLines().forEach { println(it) }
+        print(help)
         return
     }
 
@@ -32,8 +75,8 @@ fun main(args: Array<String>) {
 
     printDiff(commonSubSec, options)
     if (commonSubSec.diff.size > 1) {
-        exitProcess(1);
+        exitProcess(1)
     } else {
-        exitProcess(0);
+        exitProcess(0)
     }
 }
